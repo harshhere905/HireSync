b3ffc133e3fc6c1d0b90033eca4eb1a7bab92a7d
harshhere905 <hv96829@gmail.com>
2026-06-28 00:47:34 +0530

    added resend due to smtp ports block issue

--- Backend/src/services/email.services.js
+++ Backend/src/services/email.services.js
@@ -1,46 +1,23 @@
-import nodemailer from 'nodemailer'
-import config from '../config/email.config.js'
+import { Resend } from 'resend';
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    type: 'OAuth2',
-    user: config.EMAIL_USER,
-    clientId: config.CLIENT_ID,
-    clientSecret: config.CLIENT_SECRET,
-    refreshToken: config.REFRESH_TOKEN,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-// Verify the connection configuration
-transporter.verify((error, success) => {
-  if (error) {
-    console.error('Error connecting to email server:', error);
-  } else {
-    console.log('Email server is ready to send messages');
-  }
-});
-
-// Function to send email
 const sendEmail = async (to, subject, text, html) => {
-  try {
-    const info = await transporter.sendMail({
-      from: `"HireSync" <${config.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-      html,
+    const { data, error } = await resend.emails.send({
+        from: 'HireSync <onboarding@resend.dev>',
+        to,
+        subject,
+        html,
+        text,
     });
 
-    console.log('Message sent: %s', info.messageId);
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    return info;
-  } catch (error) {
-    console.error('Error sending email:', error);
-    throw error;
-  }
+    if (error) {
+        console.error('Email error:', error);
+        throw error;
+    }
+
+    console.log('Email sent:', data);
+    return data;
 };
 
-export default sendEmail
+export default sendEmail;
