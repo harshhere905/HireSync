51877b07c71182fb52b7654c05d9e8d34a96d04e
harshhere905 <hv96829@gmail.com>
2026-06-27 23:03:11 +0530

    fixed cors error

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,14 +2,8 @@ import express from 'express'
 import AuthRoutes from '../src/routes/auth.routes.js'
 import cookieParser from 'cookie-parser';
 import cors from 'cors'
-import { interviewRouter } from './routes/interview.routes.js';
-import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const app = express();
+import {interviewRouter} from './routes/interview.routes.js';
+const app=express();
 
 app.use(cors({
     origin: function(origin, callback) {
@@ -19,14 +13,7 @@ app.use(cors({
 }));
 app.use(express.json())
 app.use(cookieParser());
-app.use('/api/v1/auth', AuthRoutes)
-app.use('/api/v1/interview', interviewRouter)
-
-// Static files
-app.use(express.static(path.join(__dirname, "../../Frontend/dist")))
-
-app.get("/{*path}", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../Frontend/dist", "index.html"))
-})
+app.use('/api/v1/auth',AuthRoutes)
+app.use('/api/v1/interview',interviewRouter)
 
 export default app;
